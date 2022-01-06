bd2eef686ea9e11d38a0cea44aefff347d26c6ec
Jonathan Hammer <jonathan@e3software.com>
2022-01-06 14:58:32 -0700

    Handle nullable types

--- src/JsonSchema/PhpBuilder.php
+++ src/JsonSchema/PhpBuilder.php
@@ -27,6 +27,7 @@ use Swaggest\PhpCodeBuilder\Property\PatternPropertiesGetter;
 use Swaggest\PhpCodeBuilder\Property\PatternPropertySetter;
 use Swaggest\PhpCodeBuilder\Property\Setter;
 use Swaggest\PhpCodeBuilder\Types\TypeOf;
+use Swaggest\PhpCodeBuilder\Types\OrType;
 
 class PhpBuilder
 {
@@ -198,7 +199,11 @@ class PhpBuilder
                 }
 
                 if ($this->schemaIsNullable($property)) {
-                    $phpProperty->setIsMagical(true);
+//                    $phpProperty->setIsMagical(true);
+                    $or = new OrType();
+                    $or->add($phpProperty->getNamedVar()->getType());
+                    $or->add(PhpStdType::null());
+                    $phpProperty->getNamedVar()->setType($or);
                 }
 
                 if ($property->description) {
